de4027863a8a38fc90114a3574d4a69da97f0243
Rohan Sharma <rohansharma@Rohans-MacBook-Pro.local>
2024-04-20 12:31:26 -0400

    All views added

--- app/src/main/java/com/example/app/main.java
+++ app/src/main/java/com/example/app/main.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,7 @@ public class main extends AppCompatActivity {
     private ImageView coffeeImage;
     private ImageView sandwichImage;
     private ImageView donutImage;
+    private Button currOrder, allOrders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +28,22 @@ public class main extends AppCompatActivity {
         });
         coffeeImage = findViewById(R.id.coffeeImage);
         coffeeImage.setImageResource(R.drawable.coffee_main);
+
         sandwichImage = findViewById(R.id.sandwichImage);
         sandwichImage.setImageResource(R.drawable.sandwich_main);
+
         donutImage = findViewById(R.id.donutImage);
         donutImage.setImageResource(R.drawable.donut_main);
+
+        currOrder = findViewById(R.id.currOrder);
+        allOrders = findViewById(R.id.allOrders);
 
         coffeeImage.setOnClickListener(v -> openingCoffeeGUI());
         sandwichImage.setOnClickListener(v -> openingSandwichGUI());
         donutImage.setOnClickListener(v -> openingDonutGUI());
+        currOrder.setOnClickListener(v -> openingCurrentOrderGUI());
+        allOrders.setOnClickListener(v -> openingAllOrdersGUI());
+
     }
     public void openingCoffeeGUI(){
         Intent intent = new Intent(this, CoffeeGUI.class);
@@ -47,6 +57,16 @@ public class main extends AppCompatActivity {
     }
     public void openingDonutGUI(){
         Intent intent = new Intent(this, DonutGUI.class);
+//        myIntent.putExtra("key", value); //Optional parameters for transferring data between GUIs
+        main.this.startActivity(intent);
+    }
+    public void openingCurrentOrderGUI(){
+        Intent intent = new Intent(this, CurrentOrderGUI.class);
+//        myIntent.putExtra("key", value); //Optional parameters for transferring data between GUIs
+        main.this.startActivity(intent);
+    }
+    public void openingAllOrdersGUI(){
+        Intent intent = new Intent(this, AllOrdersGUI.class);
 //        myIntent.putExtra("key", value); //Optional parameters for transferring data between GUIs
         main.this.startActivity(intent);
     }
